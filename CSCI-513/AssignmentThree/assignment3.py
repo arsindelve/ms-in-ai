c3ea9bbed64345de2650ed7f6119817be365f23b
Michael Lane <michael@cosee.com>
2026-09-07 11:13:27 -0500

    Added `count_winning_team` function to compute win counts for each team and updated `go()` to display results. Refactored `initialize` to `read_file` for clarity.

--- CSCI-513/AssignmentThree/assignment3.py
+++ CSCI-513/AssignmentThree/assignment3.py
@@ -3,7 +3,7 @@
 # Assignment 3
 
 
-def initialize():
+def read_file():
     try:
         with open('nba.txt', 'r') as file:
             return file.readlines()
@@ -15,8 +15,8 @@ def initialize():
 def get_winning_team(data):
     winning_team = []
     # Skip the first line which is a header
-    for line in data[1:]:
-        line = line.strip().split('\t')
+    for next_line in data[1:]:
+        line = next_line.strip().split('\t')
         team_one, team_one_score = line[0], int(line[1])
         team_two, team_two_score = line[2], int(line[3])
         if team_one_score > team_two_score:
@@ -26,10 +26,22 @@ def get_winning_team(data):
     return winning_team
 
 
+def count_winning_team(winning_teams):
+    team_wins = {}
+    for team in winning_teams:
+        if team in team_wins:
+            team_wins[team] += 1
+        else:
+            team_wins[team] = 1
+    return team_wins
+
+
 def go():
-    data = initialize()
+    data = read_file()
     if data is not None:
-        get_winning_team(data)
+        winning_teams = get_winning_team(data)
+        winning_team_counts = count_winning_team(winning_teams)
+        print(winning_team_counts)
 
 
 if __name__ == '__main__':
