fc6364b688891fe7d9f31bb4d02a42d53f51cc75
Michael Lane <michael@cosee.com>
2026-09-07 11:04:55 -0500

    Added `get_winning_team` function to process game data and determine the winning team. Updated `go()` to utilize this logic.

--- CSCI-513/AssignmentThree/assignment3.py
+++ CSCI-513/AssignmentThree/assignment3.py
@@ -9,16 +9,28 @@ def initialize():
             return file.readlines()
     except FileNotFoundError:
         print("Error: The file 'nba.txt' was not found.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
     return None
+
+
+def get_winning_team(data):
+    winning_team = []
+    # Skip the first line which is a header
+    for lines in data[1:2]:
+        line = lines.strip().split('\t')
+        team_one = line[0]
+        team_one_score = int(line[1])
+        team_two = line[2]
+        team_two_score = int(line[3])
+        if team_one_score > team_two_score:
+            winning_team.append(team_one)
+        else:
+            winning_team.append(team_two)
 
 def go():
     data = initialize()
     if data is not None:
-        print(data)
+        get_winning_team(data)
 
 
 if __name__ == '__main__':
     go()
-
